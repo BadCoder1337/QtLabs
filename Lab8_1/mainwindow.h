ddefdd3f19fba8907b96cc804c4d15b5269ec88b
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-06-02 00:29:07 +0300

    Lab8_1 WIP

--- Lab8_1/mainwindow.h
+++ Lab8_1/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGui>
+#define maxR 5 // максимальное количество строк
+#define maxC 7 // максимальное количество столбцов
+#define HEXAGON_SIZE 20
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,12 +14,28 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void paintEvent(QPaintEvent* event);
+    void createMatrices();
+    void createNet();
+    void createPolygon(int i, int j);
+    void mousePressEvent(QMouseEvent* event);
+    void resizeEvent(QResizeEvent* event);
+
+    QPolygon **Polygon;
+    QRegion **Region;
+    QColor Color[maxR][maxC];
+    int previousRows, rows, columns;
+    double w, h;
+    double size, x0, y0, r;
+    QBrush brush;
 private:
     Ui::MainWindow *ui;
+private slots:
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
 };
 #endif // MAINWINDOW_H
